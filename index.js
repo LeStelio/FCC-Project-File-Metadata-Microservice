ee14b841d57aa8a33b00b9f1390fc5c47929bdca
S. Guerra <113545692+LeStelio@users.noreply.github.com>
2023-12-13 17:26:39 +0100

    Submission commit | Part 1

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config()
+const multer = require('multer');
+const upload = multer({ dest: './uploads' });
+const app = express();
+
+app.use(cors());
+app.use('/public', express.static(process.cwd() + '/public'));
+
+app.get('/', function (req, res) {
+  res.sendFile(process.cwd() + '/views/index.html');
+});
+
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res) => {
+  let name = req.file.originalname;
+  let type = req.file.mimetype;
+  let size = req.file.size;
+  res.json({
+    name: name,
+    type: type,
+    size: size
+  });
+})
+
+const port = process.env.PORT || 3000;
+app.listen(port, function () {
+  console.log('Your app is listening on port ' + port)
+});
